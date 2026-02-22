8088f1d9963d9a70c28aae9ccb732a6dfe8c1aef
Felipe Alves Amodio <felipeaamodio@gmail.com>
2026-02-22 13:03:33 -0300

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Felipe Alves Amodio on 22/02/26.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
