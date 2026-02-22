03391ec14f613e3acf8325e8b5593333c008427b
Felipe Alves Amodio <felipeaamodio@gmail.com>
2026-02-22 14:49:55 -0300

    builded game's frontend screen

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -9,13 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack
+        {
+            Image("background-plain")
+            
+            VStack(spacing: 50) { //using a custom spacing
+                //Logo
+                Image("logo")
+                    .scaledToFit()
+                
+                //Cards
+                HStack (spacing: 50){
+                    Image("card2")
+                        .scaledToFit()
+                    Image("card3")
+                        .scaledToFit()
+                }
+                
+                //Button
+                Button (action: {
+                    print("Button tapped")
+                }) {
+                    Image("button")
+                        .scaledToFit()
+                }
+                
+                //Score
+                HStack(spacing: 80){
+                    
+                    VStack {
+                        Text("Player").font(.headline).padding(.bottom)
+                        
+                        Text("0").font(.largeTitle)
+                    }
+                    
+                    
+                    
+                    VStack {
+                        Text("CPU").font(.headline).padding(.bottom)
+                        
+                        Text("0").font(.largeTitle)
+                    }
+                    
+                }.foregroundStyle(.white)
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
